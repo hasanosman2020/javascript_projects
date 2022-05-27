a8fcd293cc46a59c2cf5b48f9da476b0216e63b2
hasanosman2020 <hasan79.hoa@gmail.com>
2022-05-27 16:36:32 +0100

    DOCS: add html and css docs and app.js

---
+++ colour_flipper/app.js
@@ -0,0 +1,11 @@
+const colours = [
+  'red',
+  'hsl(28, 42%, 94%)',
+  'rgb(255, 255, 0)',
+  '#7cfc00',
+  'blue',
+  'hsl(275, 100%, 25%)',
+  'rgb(238, 130, 238)'
+]
+const button = document.getElementById('btn')
+const colour = document.querySelector('.colour')
