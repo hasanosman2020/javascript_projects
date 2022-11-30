eb47aa9360237c8695bf09c2c49c07c1d4d12168
hasanosman2020 <hasan79.hoa@gmail.com>
2022-11-30 16:07:48 +0000

    feat: Implemenmted the final functionality where the counter increases to the end value using the setInterval method and an increment

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -1,11 +1,25 @@
-const numbers = [...document.querySelectorAll('.number')]
-console.log(numbers)
+const items = [...document.querySelectorAll('.number')]
+console.log(items)
 
 const updateCount = el => {
-  console.log(el)
+  //console.log(el)
+  const value = parseInt(el.dataset.value)
+  //console.log(value)
+  const increment = Math.ceil(value / 10000)
+  let initialValue = 0
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment
+    if (initialValue > value) {
+      el.textContent = `${value}+`
+      clearInterval(increaseCount)
+      return
+    }
+    el.textContent = `${initialValue}`
+  }, 100)
 }
 
-numbers.forEach(number => {
-  //console.log(number)
-  updateCount(number)
+items.forEach(item => {
+  //console.log(item)
+  updateCount(item)
 })
