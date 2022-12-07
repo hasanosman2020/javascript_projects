fc6d728fe048e87337d0bab814276634932c2a59
hasanosman2020 <hasan79.hoa@gmail.com>
2022-12-07 01:38:50 +0000

    feat: Create function to display all products and function to filter products based on search terms entered by user

---
+++ filters/filters.js
@@ -0,0 +1,41 @@
+let filteredProducts = [...products]
+//console.log(products)
+
+const productsContainer = document.querySelector('.products_container')
+
+const displayProducts = () => {
+  productsContainer.innerHTML = filteredProducts
+    .map(({ id, image, title, price }) => {
+      return `<article class='product' data-id='${id}'>
+  <img
+    src='${image}'
+    alt='product'
+    class='product_img img'
+  />
+  <footer>
+    <h5 class='product_name'>'${title}'</h5>
+    <span class='product_price'>'$${price}'</span>
+  </footer>
+</article>
+`
+    })
+    .join('')
+}
+displayProducts()
+
+/* Filter by Text */
+const form = document.querySelector('.input_form')
+const searchInput = document.querySelector('.search_input')
+
+form.addEventListener('keyup', () => {
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h6>Sorry, the search has no results.</h6>`
+  }
+
+  const inputValue = searchInput.value
+  //console.log(inputValue)
+  filteredProducts = products.filter(product => {
+    return product.title.toLowerCase().includes(inputValue)
+  })
+  displayProducts()
+})
